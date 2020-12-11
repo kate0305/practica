648e9458ac33c8c72e3bb5687a018d6a352ac7af
kate0305 <72406814+kate0305@users.noreply.github.com>
2020-12-11 21:29:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,130 +1,100 @@
-/*Navigation*/
-
-const header = document.querySelector('.navigation');
-header.addEventListener('click', (event) => {
-    header.querySelectorAll('a').forEach(el => el.classList.remove('active'));
-    event.target.classList.add('active');
-});
-
-document.addEventListener('scroll', onScroll);
-function onScroll(event) {
-    const curPos = window.scrollY;
-  
-    const divs = document.querySelectorAll('body>section');
-    
-    const links = document.querySelectorAll('.navigation a');
-
-    divs.forEach((el) => {
-        if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
-            links.forEach((a) => {
-                a.classList.remove('active');
-                if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
-                    a.classList.add('active');
-                }
-            })
-        }
-    }); 
-}
+let numbers = document.querySelectorAll('.button_number'),
+    operations = document.querySelectorAll('.button_operator'),
+    clearButtons = document.querySelectorAll('.button_clear'),
+    decimalButton = document.getElementById('decimal'),
+    result = document.getElementById('result'),
+    display = document.getElementById('display'),
+    MemoryCurrentNumber = 0,
+    MemoryNewNumber = false,
+    MemoryPendingOperation = '';
 
 
-	/*Sliders*/			
-'use strict';
-    var multiItemSlider = (function () {
-      return function (selector) {
-        var
-          _mainElement = document.querySelector(selector), 
-          _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), 
-          _sliderItems = _mainElement.querySelectorAll('.slider__item'), 
-          _sliderControls = _mainElement.querySelectorAll('.slider__control'), 
-          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), 
-          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width),   
-          _positionLeftItem = 0, 
-          _transform = 0, 
-          _step = _itemWidth / _wrapperWidth * 100, 
-          _items = []; 
+for(let i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    number.addEventListener('click', (e) => {
+           numberPress(e.target.textContent);
+    });
+};
 
-        _sliderItems.forEach(function (item, index) {
-          _items.push({ item: item, position: index, transform: 0 });
-        });
+for(let i = 0; i < operations.length; i++) {
+    let operationButton = operations[i];
+        operationButton.addEventListener('click', (e) => {
+        operationPress(e.target.textContent);
+    });
+};
 
-        var position = {
-          getItemMin: function () {
-            var indexItem = 0;
-            _items.forEach(function (item, index) {
-              if (item.position < _items[indexItem].position) {
-                indexItem = index;
-              }
-            });
-            return indexItem;
-          },
-          getItemMax: function () {
-            var indexItem = 0;
-            _items.forEach(function (item, index) {
-              if (item.position > _items[indexItem].position) {
-                indexItem = index;
-              }
-            });
-            return indexItem;
-          },
-          getMin: function () {
-            return _items[position.getItemMin()].position;
-          },
-          getMax: function () {
-            return _items[position.getItemMax()].position;
-          }
-        }
+for(let i = 0; i < clearButtons.length; i++) {
+    let resetButton = clearButtons[i];
+        resetButton.addEventListener('click', (e) => {
+        clear(e.target.textContent);
+    });
+};
 
-        var _transformItem = function (direction) {
-          var nextItem;
-          if (direction === 'right') {
-            _positionLeftItem++;
-            if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
-              nextItem = position.getItemMin();
-              _items[nextItem].position = position.getMax() + 1;
-              _items[nextItem].transform += _items.length * 100;
-              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
-            }
-            _transform -= _step;
-          }
-          if (direction === 'left') {
-            _positionLeftItem--;
-            if (_positionLeftItem < position.getMin()) {
-              nextItem = position.getItemMax();
-              _items[nextItem].position = position.getMin() - 1;
-              _items[nextItem].transform -= _items.length * 100;
-              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
-            }
-            _transform += _step;
-          }
-          _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
-        }
+decimalButton.addEventListener('click', decimal);
 
-        var _controlClick = function (e) {
-          if (e.target.classList.contains('slider__control')) {
-            e.preventDefault();
-            var direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
-            _transformItem(direction);
-          }
+function numberPress(number) {
+    if (MemoryNewNumber) {
+        display.value = number;
+        MemoryNewNumber = false;
+    } else {
+        if (display.value === '0') {
+            display.value = number;
+        } else {
+            display.value += number;
         };
+    };
+};
 
-        var _setUpListeners = function () {
-          _sliderControls.forEach(function (item) {
-            item.addEventListener('click', _controlClick);
-          });
+function operationPress(op) {
+    localOperationMemory = display.value;
+        
+    if (MemoryNewNumber && MemoryPendingOperation !== '=') {
+        display.value = MemoryCurrentNumber;
+    } else {
+        MemoryNewNumber = true;
+        switch (MemoryPendingOperation) {
+            case '+':
+            MemoryCurrentNumber += +localOperationMemory;  
+            break;
+            case  '-':
+            MemoryCurrentNumber -= +localOperationMemory;
+            break;  
+            case '*' :
+            MemoryCurrentNumber *= +localOperationMemory;
+            break;  
+            case '/' :
+            MemoryCurrentNumber /= +localOperationMemory;
+            break;  
+            default:
+            MemoryCurrentNumber = +localOperationMemory;  
+        };
+            display.value = MemoryCurrentNumber;
+            MemoryPendingOperation = op;
+    };       
+};
+
+function decimal() {
+    let localDecimalMemory = display.value;
+        
+    if (MemoryNewNumber) {
+        localDecimalMemory = '0.';
+        MemoryNewNumber = false;
+    } else {
+        if (localDecimalMemory.indexOf('.') === -1) {
+            localDecimalMemory += '.'
         }
+    };
+        display.value = localDecimalMemory;
+};
 
-        _setUpListeners();
-
-        return {
-          right: function () { 
-            _transformItem('right');
-          },
-          left: function () { 
-            _transformItem('left');
-          }
-        }
-      }
-    }());
-
-	var slider = multiItemSlider('.slider')
-	console.log(slider)
+function clear(id) {
+    if (id === 'ce') {
+        display.value = '0' 
+        MemoryNewNumber = true;
+    } else if(id === 'c') {
+        display.value = '0' 
+        MemoryNewNumber = true;
+        MemoryCurrentNumber = 0,
+        MemoryPendingOperation = '';
+    };
+};
